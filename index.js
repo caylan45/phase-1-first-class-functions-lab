b5812cb0eaf48f4a0569f6f2608f741607ec43c7
caylan wilcox <caylan45@yahoo.com>
2022-11-27 19:26:10 -0600

    commit tree 751b20ab6c67a4f57226c09404b2980481625a8f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers=(array)=>{const copyarray=array.slice(0,2);
+    return copyarray;
+
+}
+const returnLastTwoDrivers=(array)=>{const copyarray=array.slice(-2);
+return copyarray
+}
+let selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+createFareMultiplier(fare)
+{
+     (num)=>{return fare*num};
+}
